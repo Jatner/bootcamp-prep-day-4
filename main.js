998cac51c590f531b6a8848219647ce926607b04
Jatner <jatner@gmail.com>
2019-01-17 19:23:16 -0800

    maped out game plan

--- main.js
+++ main.js
@@ -2,4 +2,19 @@ function greet() {
     console.log('greet funtion called')
 }
 
-greet();
+greet() {
+// get the first name input by id 
+
+
+// get values out of inputs
+
+// make gretting
+
+// display 
+
+// remove prompt
+}
+
+
+// todo: validate name inputs 
+// todo: clear inputs after greet
